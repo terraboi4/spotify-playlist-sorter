29539354eced0a975c9bc89da83bd507c1648d7f
terraboi4 <jaxstar24@gmail.com>
2025-11-03 17:40:26 -0600

    access and print basic playlist information

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Playlist Sorter
 
 ## What it does
-* Takes a Spotify playlist and sorts it by BPM, genre, or energy level
+
+- Takes a Spotify playlist and sorts it by BPM, genre, or energy level
 
 ## Done when:
+
 - [ ] Can input Spotify playlist URL
 - [ ] Fetches song data from Spotify API
 - [ ] Displays playlist sorted by user-chosen metric
 - [ ] Deployed and usable
 
+## Additional features (post v1 ship)
 
-Started: Nov 3, 2024
-Ship date: Nov 6, 2024
+- [ ] Use cluster algorithm to sort songs into "clouds"
+
+Started: Nov 3, 2025
+V1 Ship date: Nov 6, 2025
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,102 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { handleSubmit, register } = useForm();
+  const [token, setToken] = useState(Object);
+  const [playlist, setPlaylist] = useState(Object);
+
+  // long useEffect that gets access token
+  useEffect(() => {
+    async function getToken(params: { name: string; id: string }) {
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+        }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " +
+            Buffer.from(params.name + ":" + params.id).toString("base64"),
+        },
+      });
+      return await response.json();
+    }
+    const fetchData = async () => {
+      const result = await getToken({
+        name: process.env.NEXT_PUBLIC_CLIENT_ID || "",
+        id: process.env.NEXT_PUBLIC_CLIENT_SECRET || "",
+      });
+      setToken(result);
+    };
+    fetchData();
+  }, []);
+
+  const onSubmit = async (data: any) => {
+    const playlistId = data.link.match(/playlist\/([a-zA-Z0-9]+)/)[1];
+    console.log(token.access_token);
+
+    const response = await axios
+      .get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        headers: {
+          Authorization: `Bearer ${token.access_token}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        setPlaylist(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const getBpm = async (song: String, artist: String) => {
+    const options = {
+      method: "GET",
+      url: "https://track-analysis.p.rapidapi.com/pktx/analysis",
+      params: {
+        song: "Respect",
+        artist: "Aretha Franklin",
+      },
+      headers: {
+        "x-rapidapi-key": "4a4f1d0c37msh37ed5dd8d3a45fap151f18jsnd6692bd9e266",
+        "x-rapidapi-host": "track-analysis.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="text-center p-3">
+      <h1 className="text-2xl font-bold">Spotify Playlist Sorter</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-x-2">
+          <input
+            {...register("link")}
+            className="border-2 rounded-md my-4"
+            type="text"
+            placeholder="Input your playlist link here"
+          />
+          <button type="submit" className="rounded-md bg-gray-200 p-1">
+            Submit
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </form>
+
+      {playlist.items.map((song: any) => {
+        return <div key={song.id}>{song.track.name}</div>;
+      })}
     </div>
   );
 }
